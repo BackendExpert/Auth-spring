2ae65ce77ba089a698294bdc333d2084fcc551a3
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-12-09 20:18:24 +0530

    Update UserRepository.java

--- server/src/main/java/com/server/server/repository/UserRepository.java
+++ server/src/main/java/com/server/server/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.server.server.repository;
 
-public interface UserRepository {
+import com.server.server.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
